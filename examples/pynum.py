518303248c7f3bf9389077cb5fadf9c4db3d01e5
Dave Crawford <dav1dc-github@github.com>
2024-06-18 10:36:53 -0400

    save my examples working with pynum and copilot

---
+++ examples/pynum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# function to add up all of the integers in two input arrays using the pynum library but throw an error if the arrays contain strings
+def add_arrays(arr1, arr2):
+    import pynum as pn
+    if not all(isinstance(x, int) for x in arr1):
+        raise ValueError("Array 1 contains a non-integer")
+    if not all(isinstance(x, int) for x in arr2):
+        raise ValueError("Array 2 contains a non-integer")
+    return pn.add(arr1, arr2)
+
+# function to concatenate together all the string values contained in two input arrays using the pynum library
+def concat_arrays(arr1, arr2):
+    import pynum as pn
+    if not all(isinstance(x, str) for x in arr1):
+        raise ValueError("Array 1 contains a non-string")
+    if not all(isinstance(x, str) for x in arr2):
+        raise ValueError("Array 2 contains a non-string")
+    return pn.concat(arr1, arr2)
